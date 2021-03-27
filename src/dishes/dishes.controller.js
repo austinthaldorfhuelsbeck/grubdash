a6994abf7ef8236ec307a4c97a329c6084067132
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-03-26 22:34:54 -0700

    finish dishes controller router draft 1

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -1,3 +1,4 @@
+const { url } = require("inspector");
 const path = require("path");
 
 // Use the existing dishes data
@@ -6,12 +7,63 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
+// strictly middleware
+function dishExists(req, res, next) {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === Number(dishId));
+  if (foundDish) {
+    res.locals.dish = foundDish;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Dish ID not found: ${dishId}`,
+  });
+}
+
+function dishIdMatches(req, res, next) {
+  const { dishId } = req.params;
+  const { data: { newDish } = {} } = req.body;
+  if (newDish.id && newDish.id !== dishId) {
+    return next({
+      status: 400,
+      message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`,
+    });
+  }
+  next();
+}
+
+// CRUDL
 function create(req, res, next) {
-  const { data: { dish } = {} } = req.body;
+  const { data: { dishData } = {} } = req.body;
   const newDish = {
     id: nextId,
-    dish,
+    dishData,
   };
   dishes.push(newDish);
   res.status(201).json({ data: newDish });
 }
+
+function read(req, res) {
+  res.json({
+    data: res.locals.dish,
+  });
+}
+
+function update(req, res, next) {
+  const dish = res.locals.dish;
+  const { data: { newDish } = {} } = req.body;
+  dish = newDish;
+  res.json({ data: dish });
+}
+
+function list(req, res) {
+  res.json({ data: dishes });
+}
+
+module.exports = {
+  create,
+  read: [dishExists, read],
+  update: [dishIdMatches, update],
+  list,
+};
